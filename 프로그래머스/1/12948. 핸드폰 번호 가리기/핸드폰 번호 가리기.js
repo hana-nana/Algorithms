0090ae5afc1255fac2a69bdbb305cb1e97ca5e65
Hana Na <80585489+hana-nana@users.noreply.github.com>
2025-05-03 23:42:17 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,13 @@
+function solution(phone_number) {
+    let answer = '';
+    const len = phone_number.length;
+    
+    for (let i = 0; i < len; i++) {
+        if (i < len - 4) {
+            answer += '*'
+        } else {
+            answer += phone_number[i];
+        }
+    }
+    return answer;
+}
